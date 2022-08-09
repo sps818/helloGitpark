ec529dfe3e164d59034c23a10729bdd4d20d4397
sps818 <63447407@qq.com>
2022-08-10 07:53:36 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/HellogGit.java
+++ HelloGit/src/main/java/com/atguigu/HellogGit.java
@@ -3,5 +3,7 @@ package com.atguigu;
 public class HellogGit {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("更新1");
+
     }
 }
